f10a588890a4e43d023e94e95dc6a654da78e34a
LimeOrangePie <ji5489@gmail.com>
2023-01-11 19:14:12 +0900

    feat: Implemented argument parser

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
 from torch import optim, nn
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageNet
@@ -10,16 +12,26 @@ import torchmetrics
 
 from model import YOLOv1
 
-batch_size = 256
-
 
 class YOLOPretrainLitModel(LightningModule):
 
-    def __init__(self, yolo_model: YOLOv1, dataset_root: str):
+    def __init__(
+        self,
+        yolo_model: YOLOv1,
+        batch_size: int,
+        learning_rate: int,
+        num_workers: int,
+        dataset_root: str,
+    ):
         super().__init__()
         self.yolo_model = yolo_model
         self.yolo_model.pretrain_mode = True
+
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.num_workers = num_workers
         self.dataset_root = dataset_root
+
         self.train_acc = torchmetrics.Accuracy(num_classes=1000)
         self.val_acc = torchmetrics.Accuracy(num_classes=1000)
 
@@ -36,9 +48,9 @@ class YOLOPretrainLitModel(LightningModule):
                                )
                            ]))
         return DataLoader(dataset,
-                          batch_size=batch_size,
+                          batch_size=self.batch_size,
                           shuffle=True,
-                          num_workers=8)
+                          num_workers=self.num_workers)
 
     def val_dataloader(self):
         dataset = ImageNet(self.dataset_root,
@@ -52,12 +64,12 @@ class YOLOPretrainLitModel(LightningModule):
                                )
                            ]))
         return DataLoader(dataset,
-                          batch_size=batch_size,
+                          batch_size=self.batch_size,
                           shuffle=False,
-                          num_workers=8)
+                          num_workers=self.num_workers)
 
     def configure_optimizers(self):
-        return optim.Adam(self.yolo_model.parameters(), lr=3e-5)
+        return optim.Adam(self.yolo_model.parameters(), lr=self.learning_rate)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -82,15 +94,58 @@ class YOLOPretrainLitModel(LightningModule):
         self.val_acc.reset()
 
 
+def parse_args():
+    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-b',
+                        '--batch-size',
+                        default=32,
+                        type=int,
+                        help="Batch size (divided if multi-gpu)")
+    parser.add_argument('-lr',
+                        '--learning-rate',
+                        default=3e-5,
+                        type=float,
+                        help="Learning rate for optimizer")
+    parser.add_argument('-j',
+                        '--num-workers',
+                        default=4,
+                        type=int,
+                        help='Num. of dataloader worker processes')
+    parser.add_argument('--wandb-token',
+                        type=str,
+                        default='',
+                        help='Wandb token for login')
+    parser.add_argument('--no-wandb',
+                        action='store_true',
+                        help='Disable wandb integration')
+    parser.add_argument('dataset_root',
+                        type=str,
+                        help='ImageNet2012 dataset root path')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
+    args = parse_args()
+
+    if not args.no_wandb:
+        assert len(args.wandb_token
+                   ) != 0, "Put wandb token in --wandb-token argument!"
+
+        import wandb
+        wandb.login(key=args.wandb_token)
+
     model = YOLOv1(pretrain_mode=True, )
     lit_model = YOLOPretrainLitModel(
         yolo_model=model,
-        dataset_root='dataset/imagenet',
+        batch_size=args.batch_size,
+        learning_rate=args.learning_rate,
+        num_workers=args.num_workers,
+        dataset_root=args.dataset_root,
     )
 
     trainer = Trainer(
-        logger=WandbLogger(project="yolov1-imagenet"),
+        logger=True if args.no_wandb else WandbLogger(
+            project="yolov1-imagenet"),
         precision=32,
         max_epochs=200,
         accelerator='gpu',
