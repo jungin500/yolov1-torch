0dad87a3dbef8a4637a0ac75b4b0ac8bcede32cc
LimeOrangePie <ji5489@gmail.com>
2023-01-13 14:02:32 +0900

    fix: Model architecture fix and added Dropout layer

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from torch import nn
 
 
 class ModuleBuilder:
+
     def __init__(self, base_cls, *args, **kwargs):
         self.args = args
         self.kwargs = kwargs
@@ -12,10 +13,12 @@ class ModuleBuilder:
         elif self.kwargs is None:
             self.kwargs = {}
         self.base_cls = base_cls
+
     def __setattr__(self, __name, __value):
         if __name in ['args', 'kwargs', 'base_cls', 'build']:
             return super().__setattr__(__name, __value)
         self.kwargs[__name] = __value
+
     def __getattribute__(self, __name: str):
         if __name in ['args', 'kwargs', 'base_cls', 'build']:
             return super().__getattribute__(__name)
@@ -24,8 +27,10 @@ class ModuleBuilder:
             return self.args[index]
         except ValueError:
             return self.kwargs[__name]
+
     def build(self) -> nn.Module:
         return self.base_cls(*self.args, **self.kwargs)
+
 
 def build_model(arch: dict, disable_bn=False) -> nn.Module:
     model = nn.ModuleList()
@@ -45,14 +50,16 @@ def build_model(arch: dict, disable_bn=False) -> nn.Module:
             base_cls = nn.Flatten
         elif module_type == 'fc':
             base_cls = nn.Linear
+        elif module_type == 'dropout':
+            base_cls = nn.Dropout
         elif module_type == 'avgpool':
             base_cls = nn.AvgPool2d
         else:
             warnings.warn("Base class not supported: {}".format(module_type))
-            
+
         if base_cls == nn.BatchNorm2d and disable_bn:
             continue  # do not insert current layer
-            
+
         if isinstance(args, (list, tuple)):
             builder = ModuleBuilder(base_cls, *args)
         elif isinstance(args, dict):
@@ -62,7 +69,9 @@ def build_model(arch: dict, disable_bn=False) -> nn.Module:
         model.append(builder.build())
     return nn.Sequential(*model)
 
+
 class YOLOv1(torch.nn.Module):
+
     def __init__(self, pretrain_mode=False, disable_bn=True) -> None:
         super().__init__()
         backbone_arch = [
@@ -87,8 +96,9 @@ class YOLOv1(torch.nn.Module):
             ["bn", [256]],
             ["lrelu", [0.1]],
             ["conv", [256, 512, (3, 3), 1, 1]],
-            ["mp", [(2, 2), 2]],
+            ["bn", [512]],
             ["lrelu", [0.1]],
+            ["mp", [(2, 2), 2]],
             # Fig.3 Row.4
             ["conv", [512, 256, (1, 1)]],
             ["bn", [256]],
@@ -158,16 +168,18 @@ class YOLOv1(torch.nn.Module):
             # Fig.3 Row.7
             ["flatten", [1]],
             ["fc", [50176, 4096]],
-            ["lrelu", [0.1]],
-            # Fig.3 Row.8
+            ["identity", None],
+            ["dropout", 0.5],
+            # # Fig.3 Row.8
             ["fc", [4096, 1470]],
         ]
-        
+
         # original paper doesn't have bn implemented on.
         self.backbone = build_model(backbone_arch, disable_bn)
         self.head = build_model(head_arch, disable_bn)
         self.pretrain_head = build_model(pretrain_head_arch, disable_bn)
         self.pretrain_mode = pretrain_mode
+
     def forward(self, x):
         x = self.backbone(x)
         if self.pretrain_mode:
@@ -175,8 +187,17 @@ class YOLOv1(torch.nn.Module):
         else:
             x = self.head(x)
         return x
+
+
 if __name__ == '__main__':
     from torchinfo import summary
-    # summary(YOLOv1(pretrain_mode=False), input_size=(3, 448, 448), verbose=True)
-    summary(YOLOv1(pretrain_mode=True), input_size=(1, 3, 224, 224), verbose=True)
-    
+    summary(
+        YOLOv1(pretrain_mode=False),
+        input_size=(1, 3, 448, 448),
+        verbose=True,
+    )
+    # summary(
+    #     YOLOv1(pretrain_mode=True),
+    #     input_size=(1, 3, 224, 224),
+    #     verbose=True,
+    # )
